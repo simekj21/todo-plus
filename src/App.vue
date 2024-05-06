2470fd34a118511219c83c74f9e6e94233d6db1c
Jan Simek <jan.simek@konicaminolta.cz>
2024-05-06 14:20:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- first commit -->
+
+  <BackLog msg="BackLog" />
+</template>
+
+<script setup>
+import BackLog from './components/BackLog.vue'
+</script>
+
+<style scoped>
+
+</style>./components/BackLog.vue
